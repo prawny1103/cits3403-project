0261d8f806049720c1f6a0a6ec9ae80602766dd3
Prawny1103 <nucleargamer1103@gmail.com>
2026-05-17 10:22:56 +0800

    Added pages for quiz creation, updated game start logic to work with custom quizzes.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,3 @@
 from app.extensions import db
 from app.models.user import User
-from app.models.quiz import Room, RoomPlayer, Game, PlayerStats, Question
+from app.models.quiz import Room, RoomPlayer, Game, PlayerStats, Quiz, Question

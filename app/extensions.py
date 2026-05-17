777b9d401c6a7cf042ee2df183b25bd77b26b6c7
Adam Young <219706651+saykxl@users.noreply.github.com>
2026-05-17 17:41:16 +0800

    Added comments for __init__.py and extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -1,7 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+# Initialize database
 db = SQLAlchemy()
 
+# Initialize login manager
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
